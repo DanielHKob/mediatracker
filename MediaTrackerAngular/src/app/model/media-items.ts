148098fa851f83c81ab8524a113b0ac60ca6f242
DanielHKob <danielkobel37@hotmail.com>
2025-01-16 16:08:41 +0100

    Changes for interfaces

--- MediaTrackerAngular/src/app/model/media-items.ts
+++ MediaTrackerAngular/src/app/model/media-items.ts
@@ -1,2 +1,12 @@
 export interface MediaItems {
+    Id: number;
+    Title: string;
+    Type: string;
+    ReleaseYear: number;
+    Rating: number;
+    Comments: string;
+    StreamingService: string;
+    CreateDate: Date;
+    UserId: number;
+
 }
--- MediaTrackerAngular/src/app/model/user.ts
+++ MediaTrackerAngular/src/app/model/user.ts
@@ -1,2 +1,9 @@
 export interface User {
+    id: number;
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    CreateDate: Date;
+
+
 }
