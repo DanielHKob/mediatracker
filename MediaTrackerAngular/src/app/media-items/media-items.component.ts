ab411f512d5628caaf6345e460c9b2fc539b7344
DanielHKob <danielkobel37@hotmail.com>
2025-01-16 20:16:30 +0100

    Adding the feature for media-items component

--- MediaTrackerAngular/src/app/media-items/media-items.component.ts
+++ MediaTrackerAngular/src/app/media-items/media-items.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MediaItems } from '../model/media-items';
+import { MediaitemsService } from '../services/mediaitems.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-media-items',
   standalone: true,
-  imports: [],
+  imports: [MediaItemsComponent,
+    FormsModule,
+    DatePipe
+  ],
   templateUrl: './media-items.component.html',
   styleUrl: './media-items.component.css'
 })
-export class MediaItemsComponent {
+export class MediaItemsComponent  implements OnInit {
+@Input() mediaitems!: MediaItems;
+mediaform: FormGroup;
+  constructor(private mediaService: MediaitemsService,
+    private fb: FormBuilder,
+    private datePipe: DatePipe,
+    private snackbar: MatSnackBar
+  ) {
+    this.mediaform = this.fb.group({
+      id: [null, Validators.required],
+      Title: [null],
+      Type: [null],
+      ReleaseYear: [null],
+      Rating: [null],
+      Comments: [null],
+      StreamingService: [null],
+      CreateDate: [null],
+      UserId: [null]
 
+    })
+  }
+
+  ngOnInit(): void{
+    
+  }
+
+
+  fetchMedia(): void {
+    this.mediaService.getMedia(this.mediaitems.Id).subscribe((data: MediaItems) => {
+      this.mediaitems = data;
+      const formattedDate = this.datePipe.transform(this.mediaitems.CreateDate, 'yyyy-MM-dd');
+      this.mediaform.patchValue({
+        id: this.mediaitems.Id,
+        Title: this.mediaitems.Title,
+        Type: this.mediaitems.Type,
+        ReleaseYear: this.mediaitems.ReleaseYear,
+        Rating: this.mediaitems.Rating,
+        Comments: this.mediaitems.Comments,
+        StreamingService: this.mediaitems.StreamingService,
+        CreateDate: this.mediaitems.CreateDate,
+        UserId: this.mediaitems.UserId
+      })
+
+    })
+
+    }  
 }
+
--- MediaTrackerAngular/src/app/services/mediaitems.service.ts
+++ MediaTrackerAngular/src/app/services/mediaitems.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MediaItems } from '../model/media-items';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaitemsService {
+  baseUrl: string = "http://localhost:5057/api";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+ 
+  getMedias(): Observable<MediaItems[]> {
+    return this.http.get<MediaItems[]>(this.baseUrl + "/Media");
+  }
+
+  getMedia(id: number): Observable<MediaItems> {
+    return this.http.get<MediaItems>(`${this.baseUrl}/Media/${id}`);
+  }
+
+  createMedia(mediaitems: Partial<MediaItems>): Observable<MediaItems> {
+    return this.http.post<MediaItems>(`${this.baseUrl}/Media`, mediaitems);
+  }
+
+  updateMedia(id: number, mediaitem: MediaItems): Observable<any>{
+    return this.http.put(`${this.baseUrl}/Media/${id}`, mediaitem);
+  }
+
+  deleteMedia(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/Media/${id}`);
+  }
+
 }
