7d9e8636129f83f4c42901be111fbcdd50b93e6e
DanielHKob <danielkobel37@hotmail.com>
2025-01-16 15:07:08 +0100

    Adding components and model folder

---
+++ MediaTrackerAngular/src/app/media-items/media-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-items.component.html',
+  styleUrl: './media-items.component.css'
+})
+export class MediaItemsComponent {
+
+}
