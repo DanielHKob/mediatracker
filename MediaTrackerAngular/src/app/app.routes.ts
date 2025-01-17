c5e01d7c19746b474fb26a44ae92de4f0b70f421
DanielHKob <danielkobel37@hotmail.com>
2025-01-17 21:01:54 +0100

    Adding new routes and components

--- MediaTrackerAngular/src/app/app.routes.ts
+++ MediaTrackerAngular/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { MediaItemsComponent } from './media-items/media-items.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path: 'media-list', component: MediaItemsComponent}
+    {path: 'media-list', component: MediaItemsComponent},
+    { path: 'login', component: LoginComponent}, // Login route FIRST
+
+
+    // Redirect to login if no matching route
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' } // Wildcard route last
+
+
+
 ];
