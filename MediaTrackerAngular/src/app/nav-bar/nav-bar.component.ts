dbb274bcf656021311af0abfad5c1e9c42851022
DanielHKob <danielkobel37@hotmail.com>
2025-01-17 14:28:03 +0100

    Adding the nav-bar component to main branch

--- MediaTrackerAngular/src/app/nav-bar/nav-bar.component.ts
+++ MediaTrackerAngular/src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatMenuModule,MatIcon,MatToolbarModule,MatButton],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
