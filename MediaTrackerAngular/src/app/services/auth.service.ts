c47f15c04f40085e806b5c8dd11ee63eafe24b05
DanielHKob <danielkobel37@hotmail.com>
2025-01-19 20:38:49 +0100

    Updating authentication

--- MediaTrackerAngular/src/app/services/auth.service.ts
+++ MediaTrackerAngular/src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  baseUrl: string = "http://localhost:5057/api";
+
+  constructor(private http: HttpClient) {}
+
+  // Instead of "username", we now call it "identifier"
+  authenticate(identifier: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/login/authenticate`, {
+      identifier: identifier,
+      password: password
+    });
+  }
 }
