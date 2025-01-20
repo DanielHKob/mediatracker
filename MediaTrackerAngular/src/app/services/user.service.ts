387f4cb5255614b3bb862bb5d140fda3c3bf2cfa
DanielHKob <danielkobel37@hotmail.com>
2025-01-20 20:45:13 +0100

    Update user.service.ts

--- MediaTrackerAngular/src/app/services/user.service.ts
+++ MediaTrackerAngular/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User} from '../model/user';
 import { Observable } from 'rxjs';
 @Injectable({
@@ -11,31 +11,46 @@ export class UserService {
  
    constructor(private http: HttpClient) {}
   
+    // Helper method to create headers
+    private getAuthHeaders(): HttpHeaders {
+      const token = "Abc123!!!"; // Retrieve the token from localStorage or another storage
+      return new HttpHeaders({
+        'Authorization': `Bearer ${token}` // Replace 'Bearer' with your specific auth scheme if different
+      });
+    }
+
    getUsers(Id: number): Observable<User[]> {
-     return this.http.get<User[]>(this.baseUrl + "/User");
+    const headers = this.getAuthHeaders();
+     return this.http.get<User[]>(this.baseUrl + "/User", {headers});
    }
  
    getUser(id: number): Observable<User> {
-     return this.http.get<User>(`${this.baseUrl}/User/${id}`);
+    const headers = this.getAuthHeaders();
+     return this.http.get<User>(`${this.baseUrl}/User/${id}`, {headers});
    }
  
    createUser(mediaitems: Partial<User>): Observable<User> {
-     return this.http.post<User>(`${this.baseUrl}/User`, mediaitems);
+    const headers = this.getAuthHeaders();
+     return this.http.post<User>(`${this.baseUrl}/User`, mediaitems, {headers});
    }
  
    updateUser(id: number, mediaitem: User): Observable<any>{
-     return this.http.put(`${this.baseUrl}/User/${id}`, mediaitem);
+    const headers = this.getAuthHeaders(); 
+    return this.http.put(`${this.baseUrl}/User/${id}`, mediaitem, {headers});
    }
  
    deleteUser(id: number): Observable<any> {
-     return this.http.delete(`${this.baseUrl}/User/${id}`);
+    const headers = this.getAuthHeaders(); 
+    return this.http.delete(`${this.baseUrl}/User/${id}`, {headers});
    }
    
    getUserByEmail(email: string): Observable<User> {
-    return this.http.get<User>(`${this.baseUrl}/User/login/${email}`);
+    const headers = this.getAuthHeaders();
+    return this.http.get<User>(`${this.baseUrl}/User/login/${email}`, {headers});
   }
   getLatestUserId(): Observable<number> {
-    return this.http.get<number>(this.baseUrl +"/User/latest-id");
+    const headers = this.getAuthHeaders();
+    return this.http.get<number>(this.baseUrl +"/User/latest-id", {headers});
   }
 
 }
